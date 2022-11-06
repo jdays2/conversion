9f1cd0a725921ff8531fc1dfd8b764d7f0778552
MarkHW <lolka-pop@rambler.ru>
2022-11-06 15:50:45 +0300

    'homework'

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const meters = 3.281;
 const liter = 0.264;
 const kilogram = 2.204;
+
+
 const input = document.querySelector('.conversion__input');
 const conversionBtn = document.querySelector('.conversion__btn');
 const lengthConversion = document.querySelector('.conversion__card-subtitle--length');
@@ -8,8 +10,13 @@ const volumeConversion = document.querySelector('.conversion__card-subtitle--vol
 const massConversion = document.querySelector('.conversion__card-subtitle--mass');
 
 conversionBtn.addEventListener('click', () => {
-    lengthConversion.innerHTML = (Number(0) + Number(input.value) + " meters = " + (input.value * meters).toFixed(3) + " feet | " + input.value + " feet = " + (input.value / meters).toFixed(3) + " meters");
-    volumeConversion.innerHTML = (Number(0) + Number(input.value) + " liters = " + (input.value * liter).toFixed(3) + " gallons | " + input.value + " gallons = " + (input.value / liter).toFixed(3) + " liters");
-    massConversion.innerHTML = (Number(0) + Number(input.value) + " kilos = " + (input.value * kilogram).toFixed(3) + " pounds | " + input.value + " pounds = " + (input.value / kilogram).toFixed(3) + " kilos");
+    if(isNaN(input.value.replace(",", "."))){
+        alert('NOT A NUMBER! TRY AGAIN!')
+        return
+    }
+
+    lengthConversion.innerHTML = (Number(0) + Number(input.value.replace(",", ".")) + " meters = " + (input.value.replace(",", ".") * meters).toFixed(3) + " feet | " + input.value.replace(",", ".") + " feet = " + (input.value.replace(",", ".") / meters).toFixed(3) + " meters");
+    volumeConversion.innerHTML = (Number(0) + Number(input.value.replace(",", ".")) + " liters = " + (input.value.replace(",", ".") * liter).toFixed(3) + " gallons | " + input.value.replace(",", ".") + " gallons = " + (input.value.replace(",", ".") / liter).toFixed(3) + " liters");
+    massConversion.innerHTML = (Number(0) + Number(input.value.replace(",", ".")) + " kilos = " + (input.value.replace(",", ".") * kilogram).toFixed(3) + " pounds | " + input.value.replace(",", ".") + " pounds = " + (input.value.replace(",", ".") / kilogram).toFixed(3) + " kilos");
 })
 
